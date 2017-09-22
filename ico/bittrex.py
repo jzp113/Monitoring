77fae6daa95ad74d2e3dd0be0830c143e7399c00
JoshuaOpolko <mjopolko@gmail.com>
2017-09-22 16:15:39 +0000

    ico

--- ico/bittrex.py
+++ ico/bittrex.py
@@ -14,7 +14,7 @@ cursor = db.cursor()
 sql = "SELECT * FROM coins"
 
 cursor.execute(sql)
-# Fetch all the rows in a list of lists.
+# Check which coin are in the db as known_coins
 results = cursor.fetchall()
 known_coins = []
 for row in results:
@@ -23,17 +23,16 @@ for row in results:
     created = row[2]
     isactive = row[3]
     exchange = row[4]
-    # Now print fetched result
-    #print(marketcurrency, basecurrency, created, isactive, exchange )
+    
+#Append coins to list known_coins
     known_coins.append(marketcurrency)
-print(known_coins)
+#print(known_coins)
 
-#Get coins from bittrex
-
+#Get coins from bittrex to compare with know_coins
 url = ('https://bittrex.com/api/v1.1/public/getmarkets')
 r = requests.get(url)
 json_obj = json.loads(r.text)
-print(json_obj)
+#print(json_obj)
 for i in (json_obj['result']):
     marketcurrency = (i['MarketCurrency'])
     if marketcurrency not in known_coins:
@@ -41,11 +40,14 @@ for i in (json_obj['result']):
         created = (i['Created'])
         isactive = (i['IsActive'])
         exchange = 'bittrex'
-        print('nope')
-        print(marketcurrency)
-        print(basecurrency)
-        print(created)
-        print(isactive)
-        print(exchange)     
+        
+        #Insert record with new coins into ico.coins db
+        
+       #print('nope')
+        #print(marketcurrency)
+        #print(basecurrency)
+       # print(created)
+       # print(isactive)
+       # print(exchange)     
 # disconnect from server
 db.close()
