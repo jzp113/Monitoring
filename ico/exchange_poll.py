5c49a442840af26b60bb1b22193e92e77845b72b
JoshuaOpolko <mjopolko@gmail.com>
2017-09-25 16:55:57 +0000

    na

--- ico/exchange_poll.py
+++ ico/exchange_poll.py
@@ -45,9 +45,9 @@ for exchange in api_list:
             basecurrency = (i['BaseCurrency'])
             created = (i['Created'])
             isactive = (i['IsActive'])
-            name = urlparse(exchange)
+            exname = urlparse(exchange)
             # Exchange column 
-            exch = (name.netloc.replace("-","_").split(".",1)[0])
+            exch = (exname.netloc.replace("-","_").split(".",1)[0])
             marketcurrency = (i['MarketCurrency']) 
             mysql_select = "insert into coins (marketcurrency, basecurrency, created, isactive, exchange, discovered) values(%s, %s, %s, %s, %s, %s)"
             cursor.execute(mysql_select, (i['MarketCurrency'], i['BaseCurrency'], i['Created'], i['IsActive'], exch, datetime.utcnow()))
