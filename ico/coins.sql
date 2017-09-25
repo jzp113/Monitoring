2e96a780439f853877c24ae37d9f241407e36534
JoshuaOpolko <mjopolko@gmail.com>
2017-09-25 16:33:27 +0000

    name field

--- ico/coins.sql
+++ ico/coins.sql
@@ -1,2 +1,9 @@
-CREATE TABLE  coins (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,  marketcurrency VARCHAR(6), basecurrency VARCHAR(6), created datetime,  isactive VARCHAR(5),  exchange VARCHAR(16), discovered datetime);
+CREATE TABLE  coins (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,  
+marketcurrency VARCHAR(6), 
+basecurrency VARCHAR(6), 
+name VARCHAR(32), 
+created datetime,  
+isactive VARCHAR(5),  
+exchange VARCHAR(16), 
+discovered datetime);
 
