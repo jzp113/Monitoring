5c32471b17c9c66a964596b3ab6bb5f7f8f7cc93
JoshuaOpolko <mjopolko@gmail.com>
2017-09-26 00:35:09 +0000

    separated exchanges for now

---
+++ ico/bittrex.get.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+import pymysql
+import requests.packages.urllib3
+requests.packages.urllib3.disable_warnings()
+import json
+from urllib.parse import urlparse
+from datetime import datetime
+
+# db
+db = pymysql.connect("localhost","test","test","ico")
+cursor = db.cursor()
+
+select_sql = "SELECT marketcurrency FROM bittrex"
+cursor.execute(select_sql)
+results = cursor.fetchall()
+
+# Make list of coins in the db
+known_coins = []
+for row in results:
+    coin = row[0] 
+    known_coins.append(coin)
+  
+# Remove duplicates
+uniq_list = set(known_coins)
+known_coins = uniq_list
+print(len(known_coins))
+
+# Get coins to compare with known_coins
+r = requests.get('https://bittrex.com/api/v1.1/public/getmarkets')
+json_obj = json.loads(r.text)
+
+for i in (json_obj['result']):
+    marketcurrency = (i['MarketCurrency'])
+# Only making a list of coins trading in BTC for now
+    if (i['MarketCurrency']) in known_coins or ((i['BaseCurrency']) != 'BTC') :
+        pass
+    else:
+        print(marketcurrency + ' nope')
+        basecurrency = (i['BaseCurrency'])
+        created = (i['Created'])
+        isactive = (i['IsActive'])
+        name = (i['MarketCurrencyLong'])
+        # Exchange column 
+        marketcurrency = (i['MarketCurrency']) 
+        mysql_select = "insert into bittrex (marketcurrency, basecurrency, created, status, name, exchange, discovered) values(%s, %s, %s, %s, %s, %s, %s)"
+        cursor.execute(mysql_select, (i['MarketCurrency'], i['BaseCurrency'], i['Created'], i['IsActive'], name, 'bittrex', datetime.utcnow()))
+    db.commit()        
+db.close()
