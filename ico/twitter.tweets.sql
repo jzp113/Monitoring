777aa945825ca8c3bc83986dc5c96eaba167daa6
JoshuaOpolko <mjopolko@gmail.com>
2017-09-28 19:39:45 +0000

    add

---
+++ ico/twitter.tweets.sql
@@ -0,0 +1,12 @@
+CREATE TABLE twitter.tweets (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,  
+name VARCHAR(24),   
+profurl VARCHAR(36), 
+followers bigint(20),
+friends bigint(20),
+source varchar(48),
+location varchar(48),
+screen_name varchar(512),
+tweet_id bigint(20),
+tweet_at datetime,
+born datetime,
+text varchar(240));
