97ecf2e8f66cd0dce1476aa4a10eec3d4a9fa578
JoshuaOpolko <mjopolko@gmail.com>
2017-10-09 05:22:39 +0000

    added fields

--- ico/db/twitter.tweets.sql
+++ ico/db/twitter.tweets.sql
@@ -1,17 +1,30 @@
+CREATE DATABASE twitter 
+    CHARACTER SET utf8mb4
+    COLLATE utf8mb4_unicode_ci;
+
+
 CREATE TABLE twitter.tweets (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,  
-name VARCHAR(24),  
-description varchar(128), 
-profurl VARCHAR(36), 
+screen_name varchar(24),
+tweet_at datetime,
+born datetime,
+urls varchar(1024),
+symbols varchar(256),
+description varchar(320),
+username varchar(32),
+text varchar(320),
+favorite_count bigint(20),
+favorited bool,
 followers bigint(20),
 friends bigint(20),
 source varchar(48),
 location varchar(48),
-screen_name varchar(512),
+tweet_id bigint(20),
+statuses_count bigint(20),
 time_zone varchar(48),
 utc_offset varchar(24),
-tweet_id bigint(20),
-tweet_at datetime,
 user_id bigint(20),
-born datetime,
-lang varchar(8),
-text varchar(320));
+in_reply_to_screen_name varchar(512),
+in_reply_to_status_id bigint(20),
+in_reply_to_user_id bigint(20),
+retweet_count bigint(20),
+retweeted bigint(20));
--- ico/twitter/twitter.db.altcoin1.py
+++ ico/twitter/twitter.db.altcoin1.py
@@ -1,66 +1,65 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+from __future__ import absolute_import, print_function
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
-import json
-import webbrowser
-import pprint
 import MySQLdb
+import json
 import time
-import requests.packages.urllib3
-requests.packages.urllib3.disable_warnings()
-import re
-
 
 conn = MySQLdb.connect("localhost","test","test","twitter",charset="utf8mb4",init_command='SET NAMES utf8mb4')
 c = conn.cursor()
 
 # Auth
-consumer_key            =  ""
-consumer_secret         = ""
-access_token            = ""
-access_token_secret     = ""
+consumer_key            = " "
+consumer_secret         = " "
+access_token            = " - "
+access_token_secret     = " "
 
 class StdOutListener(StreamListener):
 
     def on_data(self, data):
-        try:
-            data = (data).encode("utf-8","ignore")
-            description = (json.loads(data)['user']['description'])
-            name     = json.loads(data)['user']['name']
-            text     = json.loads(data)['text']
-            screen_name = json.loads(data)['user']['screen_name']
-            profurl = ("https://twitter.com/%s" % screen_name) 
-            followers = json.loads(data)['user']['followers_count']
-            friends = json.loads(data)['user']['friends_count']
-            source = json.loads(data)['source'].split(">")[-2].replace("</a","")
-            location = json.loads(data)['user']['location']
-            tweet_id = json.loads(data)['id']
-            user_id = json.loads(data)['user']['id']
-            tweet_date = json.loads(data)['created_at']
-            user_created = json.loads(data)['user']['created_at']
-            time_zone = json.loads(data)['user']['time_zone']
-            utc_offset = json.loads(data)['user']['utc_offset']
-            tweet_at = time.strftime('%Y-%m-%d %H:%M:%S', time.strptime(tweet_date,'%a %b %d %H:%M:%S +0000 %Y'))
-            born = time.strftime('%Y-%m-%d %H:%M:%S', time.strptime(user_created,'%a %b %d %H:%M:%S +0000 %Y'))
-            text = text.lower()  
-            print(text)
-            c.execute("INSERT INTO tweets (born,tweet_at,tweet_id,user_id,screen_name,name,text,description,profurl,followers,friends,source,location,time_zone,utc_offset) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",(born,tweet_at,tweet_id,user_id,screen_name,name,text,description,profurl,followers,friends,source,location,time_zone,utc_offset))
-            conn.commit()
-            return True
-        except:
-            pass
+        screen_name = json.loads(data)['user']['screen_name']
+        screen_name = (screen_name).encode("utf-8","ignore")
+        json.loads(data)['created_at']
+        tweet_date = json.loads(data)['created_at']
+        tweet_at = time.strftime('%Y-%m-%d %H:%M:%S', time.strptime(tweet_date,'%a %b %d %H:%M:%S +0000 %Y'))
+        user_created = json.loads(data)['user']['created_at']
+        born = time.strftime('%Y-%m-%d %H:%M:%S', time.strptime(user_created,'%a %b %d %H:%M:%S +0000 %Y'))
+        user_mentions = json.loads(data)['entities']['user_mentions']
+        urls = json.loads(data)['entities']['urls']
+        urls = str(urls)
+        symbols = json.loads(data)['entities']['symbols']
+        symbols = str(symbols)
+        description = (json.loads(data)['user']['description'])
+        username     = json.loads(data)['user']['name']
+        username = str(username)
+        text     = json.loads(data)['text']
+        text = text.lower()
+        followers = json.loads(data)['user']['followers_count']
+        friends = json.loads(data)['user']['friends_count']
+        source = json.loads(data)['source'].split(">")[-2].replace("</a","")
+        location = json.loads(data)['user']['location']
+        tweet_id = json.loads(data)['id']
+        time_zone = json.loads(data)['user']['time_zone']
+        utc_offset = json.loads(data)['user']['utc_offset']
+        user_id = json.loads(data)['user']['id']
+        in_reply_to_screen_name = json.loads(data)['in_reply_to_screen_name']
+        in_reply_to_status_id = json.loads(data)['in_reply_to_status_id']
+        in_reply_to_user_id = json.loads(data)['in_reply_to_user_id']
+        retweet_count = json.loads(data)['retweet_count']
+        retweeted = json.loads(data)['retweeted']
+        c.execute("INSERT INTO tweets (screen_name,tweet_at,born,urls,symbols,description, username, text, followers, friends, source, location, tweet_id, time_zone, utc_offset, in_reply_to_screen_name, in_reply_to_status_id, in_reply_to_user_id,retweet_count,retweeted) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",([screen_name,tweet_at,born,urls,symbols,description, username, text, followers, friends, source, location, tweet_id, time_zone, utc_offset, in_reply_to_screen_name, in_reply_to_status_id, in_reply_to_user_id,retweet_count,retweeted]))
+        conn.commit()        
 
-def on_error(self, status):
-    print(status)
+    def on_error(self, status):
+        print(status)
 
 if __name__ == '__main__':
-
-
     l = StdOutListener()
     auth = OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
+
     stream = Stream(auth, l)
-    stream.filter(track=['$gd2 ', '$ntc '], async=True)
+    stream.filter(track=['basketball'])
