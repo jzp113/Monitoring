b5a85c2e47cac7a9732f3ef95285b0b3a7474c47
JoshuaOpolko <mjopolko@gmail.com>
2017-10-12 22:21:03 +0000

    add

--- ico/db/twitter.tweets.sql
+++ ico/db/twitter.tweets.sql
@@ -2,7 +2,8 @@ CREATE DATABASE twitter
     CHARACTER SET utf8mb4
     COLLATE utf8mb4_unicode_ci;
 
-CREATE TABLE twitter.tweets (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,  
+CREATE TABLE twitter.tweets (
+tweet_id bigint(20) NOT NULL PRIMARY KEY AUTO_INCREMENT,
 screen_name varchar(24),
 tweet_at datetime,
 born datetime,
@@ -15,7 +16,6 @@ followers bigint(20),
 friends bigint(20),
 source varchar(48),
 location varchar(128),
-tweet_id bigint(20),
 statuses_count bigint(20),
 time_zone varchar(48),
 utc_offset varchar(24),
@@ -23,4 +23,5 @@ user_id bigint(20),
 verified bool,
 in_reply_to_screen_name varchar(512),
 in_reply_to_status_id bigint(20),
-in_reply_to_user_id bigint(20));
+in_reply_to_user_id bigint(20),
+user_mentions varchar(1024));
