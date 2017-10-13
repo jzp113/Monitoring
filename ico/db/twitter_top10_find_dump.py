8be4ab4cd0525039170d77cfacf49b47faea830d
JoshuaOpolko <mjopolko@gmail.com>
2017-10-13 20:15:24 +0000

    asc

--- ico/db/twitter_top10_find_dump.py
+++ ico/db/twitter_top10_find_dump.py
@@ -36,11 +36,11 @@ for rt in top_rt:
     top_list.append(team) 
 #    print(team)
 #print(top_list)
-#top_list = ['VeztInc']
+top_list = ['eLTC_Network']
 for screen_name in top_list:
     alltweets = []	
     user_data = api.get_user(screen_name)
-    time.sleep(15)
+    time.sleep(5)
     new_tweets = api.user_timeline(screen_name = screen_name,count=200)
     alltweets.extend(new_tweets)
     oldest = alltweets[-1].id - 1
@@ -49,16 +49,17 @@ for screen_name in top_list:
     while len(new_tweets) > 0:
         print("getting tweets before %s" % (oldest))
         new_tweets = api.user_timeline(screen_name = screen_name,count=200,max_id=oldest)
-        time.sleep(15)
+        time.sleep(5)
         alltweets.extend(new_tweets)
         oldest = alltweets[-1].id - 1
         print("...%s tweets downloaded so far" % (len(alltweets)))
 
         for tweet in alltweets:
-            cursor.execute("INSERT IGNORE INTO twitter.tweets (tweet_id, screen_name, tweet_at, born, urls,symbols,description,text,followers,friends,source, location,statuses_count, time_zone, utc_offset, user_id, verified,in_reply_to_screen_name,in_reply_to_status_id,in_reply_to_user_id) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",(tweet.id,str(tweet.user.screen_name), tweet.created_at, user_data.created_at, str(tweet.entities['urls']).encode("utf-8","ignore"),str(tweet.entities['symbols']).encode("utf-8","ignore"), str(user_data.description).encode("utf-8","ignore"),tweet.text.encode("utf-8"), user_data.followers_count, user_data.friends_count,tweet.source,user_data.location, str(user_data.statuses_count), user_data.time_zone, user_data.utc_offset, user_data.id, user_data.verified, tweet.in_reply_to_screen_name, tweet.in_reply_to_status_id, tweet.in_reply_to_user_id))
+            print(screen_name)
+            cursor.execute("REPLACE INTO twitter.tweets (tweet_id, screen_name, tweet_at, born, urls,symbols,description,text,followers,friends,source, location,statuses_count, time_zone, utc_offset, user_id, verified,in_reply_to_screen_name,in_reply_to_status_id,in_reply_to_user_id,logged) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",(tweet.id,str(tweet.user.screen_name), tweet.created_at, user_data.created_at, str(tweet.entities['urls']).encode("ascii","ignore"),str(tweet.entities['symbols']).encode("ascii","ignore"), str(user_data.description).encode("ascii","ignore"),tweet.text.encode("utf-8"), user_data.followers_count, user_data.friends_count,tweet.source,user_data.location, str(user_data.statuses_count), user_data.time_zone, user_data.utc_offset, user_data.id, user_data.verified, tweet.in_reply_to_screen_name, tweet.in_reply_to_status_id, tweet.in_reply_to_user_id, datetime.datetime.now()))
             
             #cursor.execute("select screen_name from twitter.tweets limit 10;")
-    db.commit()
+        db.commit()
     
     # get newest 3 tweets from ico team
     #print('commit')
@@ -66,27 +67,23 @@ for screen_name in top_list:
     get_first_tweets = "select tweet_id from twitter.tweets where screen_name like '" + name + "'  order by tweet_at desc limit 3;"
     
     print('----------------------------------')
-    print(screen_name)
+    print(name)
     cursor.execute(get_first_tweets)
     first_retweets = cursor.fetchall() 
     first_tweets_list = []
     for fp in first_retweets:
         first_tweets_list.append(str(fp).replace(",","").replace("(","").replace(")",""))
     
-    print(first_tweets_list)
-
     for first_tweet in first_tweets_list:
         target_user_data = api.get_user(screen_name)
         retweeter_ids = api.retweeters(first_tweet)
         time.sleep(5)
-        print(retweeter_ids)       
         if len(retweeter_ids) > 0:
             retweeters_list = api.lookup_users(user_ids=retweeter_ids)
             
             for retweeter in retweeters_list:
                 target_alltweets = []	
-                print("retweeter is ")
-                print(retweeter.screen_name)
+                print("retweeter is: " + retweeter.screen_name)
                 target_user_data = api.get_user(retweeter.screen_name)
                 target_new_tweets = api.user_timeline(screen_name = retweeter.screen_name,count=200)
                 time.sleep(5)
@@ -100,9 +97,9 @@ for screen_name in top_list:
                     target_alltweets.extend(target_new_tweets)
                     target_oldest = target_alltweets[-1].id - 1
                     print("...%s tweets downloaded so far" % (len(target_alltweets)))
-            
+                    print("secondary")
                     for target_tweet in target_alltweets:  
-                        cursor.execute("INSERT IGNORE INTO twitter.tweets (tweet_id, screen_name, tweet_at, born, urls,symbols,description,text,followers,friends,source, location,statuses_count, time_zone, utc_offset, user_id, verified) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",(target_tweet.id,str(target_tweet.user.screen_name), target_tweet.created_at, target_user_data.created_at, str(target_tweet.entities['urls']).encode("utf-8","ignore"),str(target_tweet.entities['symbols']).encode("utf-8","ignore"), str(target_user_data.description).encode("utf-8","ignore"),target_tweet.text.encode("utf-8"), target_user_data.followers_count, target_user_data.friends_count, target_tweet.source,target_user_data.location, str(target_user_data.statuses_count), target_user_data.time_zone, target_user_data.utc_offset, target_user_data.id, target_user_data.verified))
+                        cursor.execute("REPLACE INTO twitter.tweets (tweet_id, screen_name, tweet_at, born, urls,symbols,description,text,followers,friends,source, location,statuses_count, time_zone, utc_offset, user_id, verified,in_reply_to_screen_name,in_reply_to_status_id,in_reply_to_user_id,logged) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",(target_tweet.id,str(target_tweet.user.screen_name), target_tweet.created_at, user_data.created_at, str(target_tweet.entities['urls']).encode("ascii","ignore"),str(target_tweet.entities['symbols']).encode("ascii","ignore"), str(user_data.description).encode("ascii","ignore"),target_tweet.text.encode("utf-8"), user_data.followers_count, user_data.friends_count,target_tweet.source,user_data.location, str(user_data.statuses_count), user_data.time_zone, user_data.utc_offset, user_data.id, user_data.verified, target_tweet.in_reply_to_screen_name, target_tweet.in_reply_to_status_id, target_tweet.in_reply_to_user_id, datetime.datetime.now()))
 
                     db.commit()
 db.close()
