b1f27cb3c11808cf7488a5617d94bf809417a270
JoshuaOpolko <mjopolko@gmail.com>
2017-10-24 15:56:38 +0000

    shapeshift

---
+++ ico/exchanges/poll_shapeshift.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+import pymysql
+import requests.packages.urllib3
+requests.packages.urllib3.disable_warnings()
+import json
+from urllib.parse import urlparse
+from datetime import datetime
+
+# db
+db = pymysql.connect("localhost","test","test","ico")
+cursor = db.cursor()
+
+select_sql = "SELECT symbol FROM ico.coins where exchange like 'shapeshift'"
+cursor.execute(select_sql)
+results = cursor.fetchall()
+
+# Make list of coins in the db
+known_coins = []
+for row in results:
+    coin = row[0] 
+    known_coins.append(coin)
+# Remove duplicates
+uniq_list = set(known_coins)
+known_coins = uniq_list
+print(len(known_coins))
+
+# Get coins to compare with known_coins
+r = requests.get('https://shapeshift.io/getcoins')
+json_obj = json.loads(r.text)
+add_list = ""
+for i in (json_obj):
+    symbol = i
+    print(symbol)
+    if (symbol in known_coins or symbol in add_list):
+        pass
+    else:
+        name = (symbol)
+        mysql_select = "insert into coins (symbol, name, exchange, discovered, new) values(%s, %s, %s, %s, %s)"
+        cursor.execute(mysql_select, (symbol, name, 'shapeshift', datetime.utcnow(), '1'))
+        add_list =  add_list + symbol
+    db.commit()        
+db.close()
--- ico/readme.md
+++ ico/readme.md
@@ -3,7 +3,7 @@ Currently tracking 3000+ listings across 15 exchanges with 250+ active twitter f
 
 **TODO**
 
-* check for rate limit and auto adjust number of rt's based on total # or rt'rs per status = 100+. 
+https://shapeshift.io/getcoins
 * unique:dups per team and individuals, get rid of most bots right there.
 * make list of unique ids before scan to not be hitting the same ones over and over.
 * screen_names at the top of multiple groups both leads and rts
@@ -25,4 +25,4 @@ Currently tracking 3000+ listings across 15 exchanges with 250+ active twitter f
 
 * 5 minute poll all exchanges
 * 10 minute check for new and add symbols to twitter filter
-* 2 am nightly poll for twitter activity on new icos
+* 2:00 UTC daily poll for twitter activity on new icos
